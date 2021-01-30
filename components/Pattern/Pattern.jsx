7830e7d84371cc30f8c39e90874bbffb57650379
Luke <lucasromerodb@gmail.com>
2021-01-29 21:05:45 -0300

    chore(storybook): pattern width

--- components/Pattern/Pattern.jsx
+++ components/Pattern/Pattern.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Pattern = ({width = 100, height = 100, colorName = 'blue600'}) => 
 (<svg width={`${width}px`} height={`${height}px`}>
     <defs>
-        <pattern id="pattern" width="20" height="20" viewBox="0 0 40,40" patternUnits="userSpaceOnUse">
+        <pattern id="pattern" width="15" height="15" viewBox="0 0 30,30" patternUnits="userSpaceOnUse">
             <rect id="pattern-background" width="100%" height="100%" fill="rgba(0, 0, 0, 0)"/>
-            <rect fill={`var(--${colorName})`} width="15" height="15"/>
+            <rect fill={`var(--${colorName})`} width="10" height="10"/>
         </pattern>
     </defs>
     <rect fill="url(#pattern)" height="100%" width="100%"/>

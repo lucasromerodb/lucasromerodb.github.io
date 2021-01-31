996f1c95edc15544d61b26b875e8d302c7a9c203
Luke <lucasromerodb@gmail.com>
2021-01-31 00:18:33 -0300

    chore(storybook): link component

---
+++ components/Link/Link.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const Link = styled.a`
+  text-decoration: underline;
+  font-weight: ${({ weight }) => weight || 400};
+  color: var(--${({ colorName }) => colorName || "blue700"});
+`;
+
+export default Link;
